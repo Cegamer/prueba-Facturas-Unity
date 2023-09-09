3093799f1cc45deedf1fd5027601d29bfffa9a05
Cegamer <cesarmauriciomn2012@gmail.com>
2023-09-09 18:18:28 -0500

    Cambios Nuevos

--- My project/Assets/Computador.cs
+++ My project/Assets/Computador.cs
@@ -14,7 +14,7 @@ public class Computador : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        facturaActual = new Factura();
     }
 
     // Update is called once per frame
